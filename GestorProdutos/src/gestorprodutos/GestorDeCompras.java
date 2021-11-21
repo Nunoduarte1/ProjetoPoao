27170ebd5847da9d1898d4f9483c3b93c94f8a92
emanuelrck <94234024+emanuelrck@users.noreply.github.com>
2021-11-21 16:56:08 +0000

    Update GestorDeCompras.java

--- GestorProdutos/src/gestorprodutos/GestorDeCompras.java
+++ GestorProdutos/src/gestorprodutos/GestorDeCompras.java
@@ -1,9 +1,17 @@
+//!! onde e que se ve se o desconto esta valido? na class venda quando estou a calcular o preco das compras? ou na class da promocao vejo se ela esta valida e se nao estiver envio o valor sem promocao?
+// !!!tive de acrescentar a data atual a venda pq faz sentido saber quando foi vendido e da jeito para fazer validacao do desconto
+// !! private boolean descontoValido(Promocoes promocao) {//!!!!! este metodo nao está criado, vai depender de como metemos as datas (na class venda)
+//!!! se calhar fazia mais sentido ter logo o peso nos produtos em vez da dimensao porque assim bastava usar isso nas vendas, contudo ao usar a dimensao estamos a mostrar que percebemos o conceito de polimorfismo e desempacotar objetos (tambem o poderiamos fazer com o peso)
+// alteracoes no uml na parte dos clientes, nos atributos das vendas no tipo de dados dos precos unitarios, nos metodos necessarios nas vendas e nas promocoes
 import java.util.ArrayList;
 
 public class GestorDeCompras {
     private ArrayList<Venda> vendas;
     private ArrayList<Cliente> clientes;
-    private ArrayList<Produto>produtos;
+    private ArrayList<Produtos>produtos;
+    private String dataAtual;
+
+  
 
     public static void main(String[] args) {
         GestorDeCompras gc = new GestorDeCompras();
@@ -13,6 +21,8 @@ public class GestorDeCompras {
     public GestorDeCompras() {
         vendas = new ArrayList<>();
         clientes = new ArrayList<>();
+        produtos=new ArrayList<>();
+        setDataAtual(dataAtual);//criar a funcao que vai alterar esta data atual
     }
 
     public void start() {
@@ -20,5 +30,15 @@ public class GestorDeCompras {
 
 
         System.out.println("-- FINISH --");
+    }
+    
+    
+    
+    public String getDataAtual() {
+        return dataAtual;
+    }
+
+    public void setDataAtual(String dataAtual) {
+        this.dataAtual = dataAtual;
     }
 }
