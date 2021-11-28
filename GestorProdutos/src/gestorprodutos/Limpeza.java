5eb29d11b67d81353cf0019148b3fd3b62cb06c2
emanuelrck <94234024+emanuelrck@users.noreply.github.com>
2021-11-28 20:44:21 +0000

    Update Limpeza.java

--- GestorProdutos/src/gestorprodutos/Limpeza.java
+++ GestorProdutos/src/gestorprodutos/Limpeza.java
@@ -1,33 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package gestordecompras;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 /**
- *objeto limpeza tem toxicidade, identificador, um nome, um preco por unidade, e o seu stock
- * se existir erro no valor de toxicidade (fora do intervalo [0,10]) imprime mensagem de erro e armazena o valor -1
+ *objeto limpeza tem toxicidade, identificador, um nome, um preco por unidade, e o seu stock e um array com as promocoes a que esta sujeito, 
+ * se existir erro no valor de toxicidade (fora do intervalo [0,10]) gera iInputMismatchException
  * 
  */
 public class Limpeza extends Produtos{
     private int toxicidade;
 
     /**
-     * @param toxicidade numero inteiro entre 0 e 10 representa a toxicidade do produto(se valor for invalido é imprimido mensagem de erro e o valor colocado igual a -1)
+     * @param toxicidade numero inteiro entre 0 e 10 representa a toxicidade do produto(se valor for invalido gera InputMismatchException)
      * @param identificador numero inteiro que permite destinguir inequivocamente os produtos
  * @param nome string com o nome do produto
- * @param precoUnitario numero inteiro com o preco da unidade do produto(se este valor for menor que zero é imprimida mensagem de erro e o valor e colocado igual -1)
- * @param stock numero inteiro com a quantidade do produto disponivel na loja (se este valor for menor que zero é imprimida mensagem de erro e o valor e colocado igual -1)
+ * @param precoUnitario numero inteiro com o preco da unidade do produto(se este valor for menor que zero gera InputMismatchException)
+ * @param stock numero inteiro com a quantidade do produto disponivel na loja (se este valor for menor que zero gera InputMismatchException)
+     * @param promocao darray de promocoes  que indica qual as promocoes a que o produto pode estar sujeito(pode ser null se nao tiver nenhuma promocao)
+ 
      */
-    public Limpeza(int toxicidade, int identificador, String nome, float precoUnitario, int stock) {
-        super(identificador, nome, precoUnitario, stock);
+  
+    public Limpeza(int toxicidade, int identificador, String nome, float precoUnitario, int stock, ArrayList<Promocoes> promocao) throws InputMismatchException {
+        super(identificador, nome, precoUnitario, stock, promocao);
+
+        
         setToxicidade(toxicidade);
     }
+    
 
-    public void setToxicidade(int toxicidade) {
+    public void setToxicidade(int toxicidade)throws InputMismatchException {
         if (toxicidade<0 || toxicidade>10) {
-            System.out.println(" valor invalido na toxicidade");
-            this.toxicidade=-1;
+            throw new InputMismatchException("valor de toxicidade invalido");
         }else{
         this.toxicidade = toxicidade;
         }
@@ -43,3 +45,4 @@ public class Limpeza extends Produtos{
     }
     
 }
+
