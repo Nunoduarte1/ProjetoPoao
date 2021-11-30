d1c1aeddd0f8c1fff7784ba3043b71e7aafa5fc0
emanuelrck <94234024+emanuelrck@users.noreply.github.com>
2021-11-30 22:47:29 +0000

    Update PagueTres.java

--- GestorProdutos/src/gestorprodutos/PagueTres.java
+++ GestorProdutos/src/gestorprodutos/PagueTres.java
@@ -1,35 +1,42 @@
-public class PagueMenos extends Promocoes{
-
-    public PagueMenos(int identifica,Data dataInicio, Data dataFim) {
+/**
+ *  gera uma promocao que tem data de inicio e fim
+ *  o identifica serve para ao criar os produtos saber quais as promocoes que lhe estao associadas
+ *  a promocao pague tres faz com que por cada 4 elementos apenas se pague 3
+ * 
+ */
+public class PagueTres extends Promocoes{
+/**
+ * 
+ * @param identifica
+ * @param dataInicio
+ * @param dataFim 
+ */
+    public PagueTres(String identifica,Data dataInicio, Data dataFim) {
         super(identifica,dataInicio, dataFim);
     }
- 
-        /**
+    /**
      * calcula o preco a pagar pela quantidade do produto a que esta associada e devolve esse valor
      * se for promocao pague tres, por cada 4 unidades sao pagas 3 ou seja quantidade a ser paga= quantidadetotal-(quantidadetotal%4)
      * se for promocao pague menos, por cada unidade a mais do que 1 é acumulado um desconto de 5% ate ao maximo de 50% isto é se levar 11 ou mais unidades daquele produto tenho um desconto de 50%=(quantidade-1)*5 percentagem
      * @param quantidade unidades do produto a serem compradas
-     * @param precoUnitario preco por unidade do produto em euros
-     * @return valor a ser pago pela quantidade do produto com o desconto incluido em euros
+     * @param precoUnitario preco por unidade do produto
+     * @return valor a ser pago pela quantidade do produto com o desconto incluido
      */
     @Override
-    public float calculaCustoComPromocao(int quantidade, float precoUnitario) {
-     float Desconto=(quantidade-1)*5/100;
-        if (Desconto>0.5) {
-            Desconto=(float) 0.5;
-        }
+    public float calculaCustoComPromocao(int quantidade,float precoUnitario) {
    
-     float custoComPromocao=(quantidade*precoUnitario)*(1-Desconto);//1-desconto corresponde a percentagem do valor do produto que tem de ser pago 
+     quantidade=quantidade-quantidade%4;
+     float custoComPromocao=quantidade*precoUnitario;
     
     return custoComPromocao;
-    }
+    
+ }
+
     @Override
     public String toString() {
-        return super.toString()+"PagueMenos{" + '}';
+        return super.toString()+"PagueTres{" + '}';
     }
 
-   
 
-    
     
 }
